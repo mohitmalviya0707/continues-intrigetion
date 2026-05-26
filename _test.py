4bb437a5be57c6790d0342e8b21c4bc3ea9cb1c2
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-05-26 10:53:47 +0530

    Fix CI workflow

--- _test.py
+++ _test.py
@@ -2,7 +2,7 @@ import pytest
 
 # Function to test square
 def square(n):
-    return n ** 2
+    return n * n
 
 # Function to test cube
 def cube(n):
@@ -12,22 +12,26 @@ def cube(n):
 def fifth_power(n):
     return n ** 5
 
+
 # Testing the square function
 def test_square():
-    assert square(2) == 4, "Test Failed: Square of 2 should be 4"
-    assert square(3) == 9, "Test Failed: Square of 3 should be 9"
+    assert square(2) == 4
+    assert square(3) == 9
+
 
 # Testing the cube function
 def test_cube():
-    assert cube(2) == 8, "Test Failed: Cube of 2 should be 8"
-    assert cube(3) == 27, "Test Failed: Cube of 3 should be 27"
+    assert cube(2) == 8
+    assert cube(3) == 27
+
 
 # Testing the fifth power function
 def test_fifth_power():
-    assert fifth_power(2) == 32, "Test Failed: Fifth power of 2 should be 32"
-    assert fifth_power(3) == 243, "Test Failed: Fifth power of 3 should be 243"
+    assert fifth_power(2) == 32
+    assert fifth_power(3) == 243
+
 
 # Test for invalid input
 def test_invalid_input():
     with pytest.raises(TypeError):
-        square("string")
+        square(None)
